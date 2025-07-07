e5f9b5dd107ebf2feb2f0f6f65fb1d7f99dc1644
80casper08 <pavlo20202020@gmail.com>
2025-07-07 21:58:55 +0300

    main.py

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from flask import Flask
 from threading import Thread
 from dotenv import load_dotenv
 from questions import op_questions, general_questions, lean_questions, qr_questions
-from hard_questions_extra import hard_questions
+from questions import hard_questions
+from hard_questions_extra import hard_questions as hard_questions_extra
+
 hard_questions += hard_questions_extra
+
 
 # Завантаження токена
 load_dotenv()
