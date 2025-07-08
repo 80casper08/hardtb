92e1172904568af2ba1541aad93fb0cb32b002e3
80casper08 <pavlo20202020@gmail.com>
2025-07-08 10:16:03 +0300

    main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQu
 from flask import Flask
 from threading import Thread
 from dotenv import load_dotenv
-from questions import op_questions, general_questions, lean_questions, hard_questions
+from questions import op_questions, general_questions, lean_questions, qr_questions
 
 load_dotenv()
 TOKEN = os.getenv("token")
@@ -44,7 +44,7 @@ sections = {
     "🦺 ОП": op_questions,
     "📚 Загальні": general_questions,
     "⚙️ LEAN": lean_questions,
-    "💪 Hard Test": hard_questions,
+    "🎲QR🎲": qr_questions,
 }
 
 def main_keyboard():
